58cb12d6ac11d30f94a1be2b5863ac6b1ac3e3d2
Michael Nunan <michael.nunan@outlook.com>
2024-03-02 10:40:22 -0600

    updated participant_guest docstring

--- coordinatesapi/models/participant_guest.py
+++ coordinatesapi/models/participant_guest.py
@@ -11,7 +11,8 @@ from .participant import Participant
 class ParticipantGuest(models.Model):
     '''This model does not have much impact on CRUD
     and serves an informational role for a user
-    to aid seating decision-making'''
+    to aid seating decision-making. This model is the ony
+    join table that has direct views'''
     guest = models.ForeignKey(Guest, on_delete=models.CASCADE, related_name='participant_guests')
     participant = models.ForeignKey(Participant, on_delete=models.CASCADE, related_name='guest_participants')
     family = models.BooleanField(null=True)
